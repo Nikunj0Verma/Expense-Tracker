b0a5f07ca9fa4f526f426fb59bd62b21fc911b8f
Nikunj <verma0025nikunj@gmail.com>
2025-11-25 10:34:22 +0530

    O

--- backend/app.js
+++ backend/app.js
@@ -6,31 +6,18 @@ const errorHandler = require("./middlewares/errorHandlerMiddleware");
 const categoryRouter = require("./routes/categoryRouter");
 const transactionRouter = require("./routes/transactionRouter");
 const app = express();
+require('dotenv').config();
 
-const uri = "mongodb+srv://nik:tgko0025@cluster0.nwqqfj8.mongodb.net/?appName=Cluster0";
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-async function run() {
-  try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
-  }
-}
-run().catch(console.dir);
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log(err));
 
-// mongoose
-// .connect("mongodb://localhost:27017/")
-// .then(() => console.log("MongoDB connected"))
-// .catch((err) => console.log(err));
-
-// const corsOptions={
-//     origin: ['http://localhost:5173'],
-// };
-// app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*', // or use '*' for all origins (not recommended for production)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 
 app.use(express.json());
